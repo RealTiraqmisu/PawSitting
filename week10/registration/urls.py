d9c90bd4d757abebc0f9eb262ab9ff19205c9536
Tiraqmisu <141468714+RealTiraqmisu@users.noreply.github.com>
2025-09-17 18:20:08 +0700

    Add files via upload

---
+++ week10/registration/urls.py
@@ -0,0 +1,27 @@
+# from django.urls import path
+
+# from . import views
+
+# urlpatterns = [
+#     # ex: /polls/
+#     path("", views.index, name="index"),
+#     # ex: /polls/5/
+#     path("<int:question_id>/", views.detail, name="detail"),
+#     # ex: /polls/5/vote/
+#     path("<int:question_id>/vote/", views.vote, name="vote"),
+# ]
+
+from django.urls import path
+from . import views
+urlpatterns = [
+    path("student/", views.StudentView.as_view(), name="student_list"),
+    path("professor/", views.ProfessorView.as_view(), name="professor_list"),
+    path("course/", views.CourseView.as_view(), name="course_list"),
+    path("faculty/", views.FacultyView.as_view(), name="faculty_list"),
+    path("nav/", views.NavView.as_view(), name="nav"),
+    path("create_student/", views.CreateStudentView, name="create_student"),
+    path("create_course/", views.CreateCourseView, name="create_course"),
+    # path("thanks/", views.thanks, name="thanks"),
+    path("update/<str:student_id>/", views.UpdateStudent, name="update_student"),
+    path("updatecourse/<str:course_code>/", views.UpdateCourse, name="update_course")
+    ]
